229997d3245e9c72524a383bc2e13033f48576cd
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-22 02:34:44 +0530

    completed uc1

---
+++ src/main/java/org/bridgelabz/addressbook/entity/Entity.java
@@ -0,0 +1,15 @@
+package org.bridgelabz.addressbook.entity;
+
+import java.util.ArrayList;
+
+public class Entity {
+    public static ArrayList<String> first_name = new ArrayList<String>();
+    public static ArrayList<String> last_name = new ArrayList<String>();
+    public static ArrayList<String> address = new ArrayList<String>();
+    public static ArrayList<String> city = new ArrayList<String>();
+    public static ArrayList<String> state = new ArrayList<String>();
+    public static ArrayList<String> email = new ArrayList<String>();
+    public static ArrayList<String> zip = new ArrayList<String>();
+    public static ArrayList<String> phone_number = new ArrayList<String>();
+
+}
