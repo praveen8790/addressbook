8293f44f9f6f1aaaf51dfc22ad6426f09e37cee2
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-23 11:47:33 +0530

    removed the files as i did program in wrong way

--- src/main/java/org/bridgelabz/addressbook/controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package org.bridgelabz.addressbook.controller;
-
-public interface Controller {
-    int menu1();
-    void menu2(int location);
-    void menu3(int location);
-    void menu5(int location);
-
-}
